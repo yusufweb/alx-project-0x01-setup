fe7ba83d0a850528070516b0812a5de0149cfdc1
yusufweb <yusuffajao4@gmail.com>
2025-06-01 14:15:11 +0100

    Updated the button component and the footer

---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gradient-to-r from-blue-500 to-purple-500 text-white shadow-md">
+      <h1>Alx Application</h1>
+    </footer>
+  );
+};
+
+export default Footer;
